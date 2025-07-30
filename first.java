38e5179a5d6d10bfd3b82c319d99d1056ed758b8
Rohini-R-Bhadange <rbhadange37@gmail.com>
2025-07-30 18:25:40 +0530

    This is second commit

--- first.java
+++ first.java
@@ -2,6 +2,6 @@
 public class first {
 
     public static void main(String[] args) {
-        System.out.println("I am Currently Learning Git");
+        System.out.println("I am Currently Learning Git and GitHub");
     }
 }
--- second.java
+++ second.java
@@ -3,5 +3,6 @@ public class second {
 
     public static void main(String[] args) {
         System.out.println("Hello here is updated statment");
+        System.out.println("I am learning Git with GitHub");
     }
 }
