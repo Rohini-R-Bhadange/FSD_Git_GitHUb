44daf99dfd1665d3b024889a64556cfab3e1ff0f
Rohini-R-Bhadange <rbhadange37@gmail.com>
2025-08-01 17:41:51 +0530

    first commit on newBranch

--- first.java
+++ first.java
@@ -2,6 +2,6 @@
 public class first {
 
     public static void main(String[] args) {
-        System.out.println("I am Currently Learning Git and GitHub");
+        System.out.println("I am Currently Learning Git and GitHub version control");
     }
 }
