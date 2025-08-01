ae01785809fac0ba1f63fdb3410cad28a828cbb9
Rohini Bhadange <136925863+Rohini-R-Bhadange@users.noreply.github.com>
2025-08-01 18:14:14 +0530

    Update first.java

--- first.java
+++ first.java
@@ -4,5 +4,6 @@ public class first {
     public static void main(String[] args) {
         System.out.println("I am Currently Learning Git and GitHub version control");
         System.out.println("This is my first git and GitHub Program");
+        System.out.println("This is for checking pull command");
     }
 }
